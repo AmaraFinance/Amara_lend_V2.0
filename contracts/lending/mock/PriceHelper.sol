4d747c425b102e601d008fabd70268364c21dbef
Amara <grace4tamp@gmail.com>
2022-09-16 15:24:50 +0800

    update get price from dex .

--- contracts/lending/mock/PriceHelper.sol
+++ contracts/lending/mock/PriceHelper.sol
@@ -54,7 +54,39 @@ contract PriceHelper is Ownable {
         }
         return false;
     }
+    /// @dev Return the value of the given input as ETH per unit.
+    /// @param pair The Uniswap pair to check the value.
+    function getLpPriceAsUsdFromUniswap(address pair) public view returns (uint) {
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+        uint totalSupply = IUniswapV2Pair(pair).totalSupply();
+        (uint r0, uint r1, ) = IUniswapV2Pair(pair).getReserves();
+        uint sqrtK = HomoraMath.sqrt(r0.mul(r1)).fdiv(totalSupply); // in 2**112
+        uint px0 = getTokenPriceAsUsd(token0);
+        uint px1 = getTokenPriceAsUsd(token1);
+        // fair token0 amt: sqrtK * sqrt(px1/px0)
+        // fair token1 amt: sqrtK * sqrt(px0/px1)
+        // fair lp price = 2 * sqrt(px0 * px1)
+        // split into 2 sqrts multiplication to prevent uint overflow (note the 2**112)
+        return sqrtK.mul(2).mul(HomoraMath.sqrt(px0)).div(2**56).mul(HomoraMath.sqrt(px1)).div(2**56);
+    }
 
+    /// @dev Return the value of the given input as ETH per unit.
+    /// @param pair The Pancakeswap pair to check the value.
+    function getLpPriceAsUsdFromPancakeswap(address pair) public view returns (uint) {
+        address token0 = IPancakePair(pair).token0();
+        address token1 = IPancakePair(pair).token1();
+        uint totalSupply = IPancakePair(pair).totalSupply();
+        (uint r0, uint r1, ) = IPancakePair(pair).getReserves();
+        uint sqrtK = HomoraMath.sqrt(r0.mul(r1)).fdiv(totalSupply); // in 2**112
+        uint px0 = getTokenPriceAsUsd(token0);
+        uint px1 = getTokenPriceAsUsd(token1);
+        // fair token0 amt: sqrtK * sqrt(px1/px0)
+        // fair token1 amt: sqrtK * sqrt(px0/px1)
+        // fair lp price = 2 * sqrt(px0 * px1)
+        // split into 2 sqrts multiplication to prevent uint overflow (note the 2**112)
+        return sqrtK.mul(2).mul(HomoraMath.sqrt(px0)).div(2 ** 56).mul(HomoraMath.sqrt(px1)).div(2 ** 56);
+    }
     /**
      * @param _proxy https://docs.chain.link/docs/reference-contracts/
      * Returns the latest price
